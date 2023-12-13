bc55676ffe02ce82a4a8716bfbcd8a3256df5ecb
Anibal0796HV <54823274+Anibal0796HV@users.noreply.github.com>
2023-12-13 12:15:21 -0500

    Add files via upload

---
+++ assets/js/main.js
@@ -0,0 +1,29 @@
+/*=============== SHOW MENU ===============*/
+
+
+/*=============== REMOVE MENU MOBILE ===============*/
+
+
+/*=============== CHANGE BACKGROUND HEADER ===============*/
+
+
+/*=============== SHOW SCROLL UP ===============*/ 
+
+
+/*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
+
+
+/*=============== PARALLAX ===============*/
+let parrallax = new Rellax('.parrallax')
+
+/*=============== GSAP ANIMATION ===============*/
+gsap.from('.home__village', 1.2, {opacity: 0, y: 100, daly: .1})
+gsap.from('.home__pine', 1.2, {opacity: 0, y: 150, daly: .3})
+gsap.from('.home__mountain-2', 1.2, {opacity: 0, y: 150, daly: .5})
+gsap.from('.home__mountain-3', 1.2, {opacity: 0, y: -150, daly: .6})
+gsap.from('.home__mountain-1', 1.2, {opacity: 0, y: 250, daly: .7})
+gsap.from('.home__moon', 1.2, {opacity: 0, y: 200, daly: .8})
+gsap.from('.home__trineo', 1.2, {opacity: 0, y: -200, daly: .8})
+gsap.from('.home__title', 1.2, {opacity: 0, y: -60, daly: 1})
+
+/*=============== SCROLL REVEAL ANIMATION ===============*/
